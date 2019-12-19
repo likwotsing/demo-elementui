f06d9f339e2a11d0ef12c3efda03bc3810f22678
likwotsing <likwotsing@gmail.com>
2019-12-19 11:22:00 +0800

    实现select和tree的联动效果

--- src/views/SelectTree.vue
+++ src/views/SelectTree.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="st">
-    <el-select class="select" v-model="value1" multiple placeholder="请选择" :popper-append-to-body="false" @visible-change="handleVisibleChange" @blur="handleBlur" @focus="handleFocus">
-      <el-option value="1" disabled="">提示：以下可以多选</el-option>
+    <el-select
+      class="select"
+      v-model="value1"
+      multiple
+      placeholder="请选择"
+      :popper-append-to-body="false"
+      @visible-change="handleVisibleChange"
+    >
       <el-tree
         class="tree"
         :class="{'active': isActive}"
@@ -10,68 +16,92 @@
         node-key="id"
         :props="defaultProps"
         show-checkbox
-        @node-click="handleNodeClick"
         @check="handleCheck"
       ></el-tree>
+      <el-option :value="Math.random()" disabled="">请选择</el-option>
+      <el-option
+        v-for="item of selectLeafNodes"
+        :key="item.id"
+        :value="item.id"
+      >提示：以下可以多选</el-option>
     </el-select>
-    <div class="tag-wrap">
-      <el-tag
-        size="mini"
-        v-for="tag in selectLeafNodes"
-        :key="tag.label"
-        closable
-        :disable-transitions="false"
-        @close="handleTagClose(tag)">
-        {{tag.label}}
-      </el-tag>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SelectTree',
+  computed: {
+    value1: {
+      get: function () {
+        // 设置tag
+        return this.selectLeafNodes.map((c, i, a) => {
+          return c.label
+        })
+      },
+      set: function (val) {
+        // 设置tree
+        // 从选中的node里筛选出全部的val，用筛选的结果设置tree
+        var flag = false
+        // 删除点击的tag
+        // selectLeafNodes需要改变, value1会跟着变化
+        this.selectLeafNodes = this.selectLeafNodes.filter((c) => {
+          flag = false
+          for (var i = 0; i < val.length; i++) {
+            if (c.label === val[i]) {
+              flag = true
+              break
+            }
+          }
+          if (flag) {
+            return c
+          }
+        })
+        this.$refs.tree.setCheckedNodes(this.selectLeafNodes)
+      }
+    }
+  },
   data () {
     return {
-      value1: [],
       isActive: false,
-      checkedNodes: [],
       selectLeafNodes: [],
-      data: [{
-        id: 1,
-        label: '一级 1',
-        children: [{
-          id: 4,
-          label: '二级 1-1',
+      data: [
+        {
+          id: 1,
+          label: '一级 1',
           children: [{
-            id: 9,
-            label: '三级 1-1-1'
-          }, {
-            id: 10,
-            label: '三级 1-1-2'
+            id: 4,
+            label: '二级 1-1',
+            children: [{
+              id: 9,
+              label: '三级 1-1-1'
+            }, {
+              id: 10,
+              label: '三级 1-1-2'
+            }]
           }]
-        }]
-      }, {
-        id: 2,
-        label: '一级 2',
-        children: [{
-          id: 5,
-          label: '二级 2-1'
         }, {
-          id: 6,
-          label: '二级 2-2'
-        }]
-      }, {
-        id: 3,
-        label: '一级 3',
-        children: [{
-          id: 7,
-          label: '二级 3-1'
+          id: 2,
+          label: '一级 2',
+          children: [{
+            id: 5,
+            label: '二级 2-1'
+          }, {
+            id: 6,
+            label: '二级 2-2'
+          }]
         }, {
-          id: 8,
-          label: '二级 3-2'
-        }]
-      }],
+          id: 3,
+          label: '一级 3',
+          children: [{
+            id: 7,
+            label: '二级 3-1'
+          }, {
+            id: 8,
+            label: '二级 3-2'
+          }]
+        }
+      ],
       defaultProps: {
         children: 'children',
         label: 'label'
@@ -86,14 +116,7 @@ export default {
     handleVisibleChange (val) {
       this.isActive = val
     },
-    handleBlur (a) {
-    },
-    handleFocus (a) {
-    },
-    handleNodeClick (data) {
-    },
     handleCheck (checkedNodes, checkedKeys, halfCheckedNodes, halfCheckedKeys) {
-      this.checkedNodes.push(checkedNodes)
       this.selectLeafNodes = this.$refs.tree.getCheckedNodes().filter((c, i, a) => c.children ? '' : c)
     }
   }
@@ -103,19 +126,14 @@ export default {
 <style lang="stylus" scoped>
 .st
   position: relative;
-  width: 200px;
+  width: 300px;
   margin: 50px auto;
   .select
     display: block;
+    >>> .el-select-dropdown__item
+      display: none;
   .tree
     display: none;
   .active
     display: block;
-  .tag-wrap
-    position: absolute;
-    top: 10px;
-    left: 6px;
-    max-width: 100%;
-    >>> .el-tag
-      margin-right: 5px;
 </style>
